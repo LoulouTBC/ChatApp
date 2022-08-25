7a3f56db0d72e5ab3694cf44a73442ac38c6da99
LoulouTBC <loulouibrahim21@gmail.com>
2022-08-25 13:33:52 +0300

    chat design/build gradle

--- lib/chat.dart
+++ lib/chat.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final _fireStore = FirebaseFirestore.instance;
+late User signedInUser; //in all the file
+
 class Chat extends StatefulWidget {
   static const screenRoute = 'chat';
 
@@ -13,9 +16,9 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
-  final _fireStore = FirebaseFirestore.instance;
+  final myController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  late User signedInUser;
+  // late User signedInUser;
   String? message;
 
   @override
@@ -37,6 +40,24 @@ class _ChatState extends State<Chat> {
     }
   }
 
+  // void getMssages() async {
+  //   final messages = await _fireStore.collection('messages').get();
+  //   // get return snapshot(copy of database at the moment)
+  //   for (var message in messages.docs) {
+  //     // for لأنو بدي كل رسالة لحال
+  //     //
+  //     print(message.data());
+  //   }
+  // }
+
+  // void messageStream() async {
+  //   await for (var snapshot in _fireStore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,6 +78,7 @@ class _ChatState extends State<Chat> {
               onPressed: () {
                 _auth.signOut();
                 Navigator.pop(context);
+                // messageStream();
               },
               icon: const Icon(Icons.logout))
         ],
@@ -67,7 +89,8 @@ class _ChatState extends State<Chat> {
             MainAxisAlignment.spaceBetween, //مشان ينزل حسب المسافة لتحت
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(),
+          // Container(),مشان المسافة وبطل بدي ياه
+          MyStreamBuilder(),
           Container(
             decoration: const BoxDecoration(
               border: Border(
@@ -79,6 +102,7 @@ class _ChatState extends State<Chat> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: myController,
                     onChanged: (value) {
                       message = value;
                     },
@@ -91,9 +115,12 @@ class _ChatState extends State<Chat> {
                 ),
                 TextButton(
                   onPressed: () {
+                    myController.clear();
+
                     _fireStore.collection('messages').add({
                       'text': message,
                       'sender': signedInUser.email,
+                      'time': FieldValue.serverTimestamp()
                     });
                   },
                   child: Text(
@@ -110,6 +137,97 @@ class _ChatState extends State<Chat> {
           )
         ],
       )),
+    );
+  }
+}
+
+class MyStreamBuilder extends StatelessWidget {
+  const MyStreamBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _fireStore.collection('messages').orderBy('time').snapshots(),
+        builder: (context, snapshot) {
+          List<MessageForm> messageWidgets = [];
+          if (snapshot.hasData) {
+            // ! اذا مافي داتا يعني
+            // add a spinner
+
+            final messages = snapshot
+                .data!.docs.reversed; //reversed to make new messages down
+            for (var message in messages) {
+              final messageText = message.get('text');
+              final messageSender = message.get('sender');
+              final currentuser = signedInUser.email;
+
+              // if (currentuser == messageSender {
+              //   //the code of the message from the signed in user
+
+              // }
+              final messageWidget = MessageForm(
+                text: messageText,
+                sender: messageSender,
+                isme: currentuser == messageSender, //if statement
+              );
+              messageWidgets.add(messageWidget);
+            }
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(backgroundColor: Colors.blue),
+            );
+          }
+          return Expanded(
+            child: ListView(
+              reverse: true, //but new messages still at top
+              children: messageWidgets,
+            ),
+          );
+        });
+  }
+}
+
+class MessageForm extends StatelessWidget {
+  const MessageForm({this.text, this.sender, required this.isme, Key? key})
+      : super(key: key);
+  final bool isme;
+  final String? sender;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isme == true ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text('$sender',
+              style: TextStyle(fontSize: 12, color: Colors.yellow[900])),
+          Material(
+            elevation: 5,
+            borderRadius: isme == true
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30))
+                : const BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30)),
+            color: isme == true ? Colors.blue[800] : Colors.white30,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: isme == true ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,30 @@ import 'package:chat/signin.dart';
 import 'package:flutter/material.dart';
 import 'welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'my chat app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       // home: Register(),
-      initialRoute: Welcome.screenRoute,
+      initialRoute:
+          _auth.currentUser != null ? Chat.screenRoute : Welcome.screenRoute,
       routes: {
         Welcome.screenRoute: (context) => const Welcome(),
         SignIn.screenRoute: (context) => const SignIn(),
